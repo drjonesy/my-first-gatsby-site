675026c9ba709bbf91922ebd07c53c42ac438a80
ryan <drjonesy@users.noreply.github.com>
2023-11-06 13:59:34 -0800

    Added Links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import * as React from "react";
+import { Link } from 'gatsby';
 
 const IndexPage = () => {
   return (
     <main>
       <h1>Welcome to Gastby site!</h1>
+      <Link to="/about">About</Link>
       <p>I'm making this by following the Gastby tutorial.</p>
     </main>
   );
